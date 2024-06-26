2a42e6a6dab291508858b8842c411016b4118ad8
cryproot <65208096+cryproot@users.noreply.github.com>
2024-06-26 14:15:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-from busqueda import buscar_s3_public_access  # Importar la función del script busqueda.py
+from busqueda import buscar_s3_public_access  # Importar función de busqueda.py para S3
+from busqueda2 import buscar_ec2_configuraciones  # Importar función de busqueda2.py para EC2
 import json
 
 app = Flask(__name__)
@@ -20,6 +21,13 @@ def buscar_s3():
     resultados_json = buscar_s3_public_access()
     resultados = json.loads(resultados_json)
     return render_template('resultados.html', resultados=resultados)
+
+# Ruta para ejecutar la búsqueda de EC2 y mostrar los resultados
+@app.route('/buscar_ec2')
+def buscar_ec2():
+    resultados_json = buscar_ec2_configuraciones()
+    resultados = json.loads(resultados_json)
+    return render_template('resultadosec2.html', resultados=resultados)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3000, debug=True)
